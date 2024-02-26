a6ef6c4530465e398c45bafcca8f2688e17af050
Nirmal J13 <nirmalnj2003@gmail.com>
2024-02-26 22:15:07 +0530

    Added resume

--- frontend/src/Components/TopContent/TopContent.js
+++ frontend/src/Components/TopContent/TopContent.js
@@ -7,7 +7,7 @@ export const TopContent = () => {
         <div className='topcontent__container'>       
             <h1>Mr.Nirmal J</h1>       
            <p>A Professional Web Developer</p>
-           <a href=''>
+           <a href='/resume/Nirmal J-Resume.pdf' download="Nirmal J-Resume.pdf">
             <button className='topcontent__downloadbutton'>Download Resume</button>
            </a>
            <Link to="projects" smooth={true} duration={500}>
