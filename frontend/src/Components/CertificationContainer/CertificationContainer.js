6eaa260e86cf4bb74bf3864d8ccd1b4d9df05880
Nirmal J13 <nirmalnj2003@gmail.com>
2024-03-06 22:08:51 +0530

    Added links

--- frontend/src/Components/CertificationContainer/CertificationContainer.js
+++ frontend/src/Components/CertificationContainer/CertificationContainer.js
@@ -2,37 +2,50 @@ import React from 'react'
 import { Element } from 'react-scroll'
 import './CertificationContainer.css'
 import { Certifications } from '../Certifications/Certifications'
+import { Link } from '@mui/material'
 export const CertificationContainer = () => {
     const certfify=[
         {
           number:1,
           title:"Programming in Java",
           desc:"Cerified from NPTEL platform",
+          link:'https://drive.google.com/file/d/1MbbFgwvRswSd3fusNjfonA9p-1rgqfBU/view?usp=drive_link',
         },
         {
           number:2,
-          title:"HTML5 and CSS3 in Advance",
-          desc:"Certified from infosysspringboard"
+          title:"HTML5 - The Language",
+          desc:"Certified from infosysspringboard",
+          link:'https://drive.google.com/file/d/1fU67864FdTYeVf7t8ZdAs8_qibh2rdmX/view?usp=drive_link'
         },
         {
           number:3,
-          title:"React.js Web Developer",
-          desc:"Certified from infosysspringboard"
+          title:"CSS3 - Cascading Style Sheet",
+          desc:"Certified from infosysspringboard",
+          link:'https://drive.google.com/file/d/1UyrkzwKmdeWG4xHxnN8LpLgWrRJNxqfv/view?usp=drive_link'
         },
         {
           number:4,
-          title:"TechA Javascript Certification",
-          desc:"Certified from infosysspringboard"
+          title:"React.js Web Developer",
+          desc:"Certified from infosysspringboard",
+          link:'https://drive.google.com/file/d/1gO_JjsnugLs_geKIdjjMobXpXr7rkwJ8/view?usp=drive_link'
         },
         {
           number:5,
-          title:"Problem Solving Skills",
-          desc:"Certified from Hackerrank"
+          title:"TechA Javascript Certification",
+          desc:"Certified from infosysspringboard",
+          link:'https://drive.google.com/file/d/1JohMHRBB5q-RKgwjpWNVXoKYmt0RLDp4/view?usp=drive_link'
         },
         {
           number:6,
+          title:"Problem Solving Skills",
+          desc:"Certified from Hackerrank",
+          link:'https://drive.google.com/file/d/1gmDhoBPjx3lsuSEu7FwlU_Hw5z0FGwXP/view?usp=drive_link'
+        },
+        {
+          number:7,
           title:"MERN Stack Web Development",
-          desc:"Certified from Udemy"
+          desc:"Certified from Udemy",
+          link:'https://drive.google.com/file/d/1sUMlnlmW1AGC5GCDTcJKdYoyLJa50V-E/view?usp=drive_link'
         }
       ]
   return (
@@ -42,12 +55,14 @@ export const CertificationContainer = () => {
         {
             certfify.map((x,i)=>{
                 return(
-                <Certifications 
+                <a  href={x.link} className='links'>
+                  <Certifications 
                         key={i} 
                         number={x.number}
                         title={x.title} 
-                        desc={x.desc} 
+                        desc={x.desc}
                     />     
+                </a>
             )})
         }
     </div>
